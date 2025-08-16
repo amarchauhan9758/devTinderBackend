fed2e3d61d7d1ddebe11d12e0fd56bb0d5d19f21
amarchauhan9758 <aryanchauhan9758@gmail.com>
2025-08-16 16:31:56 +0530

    code commit

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const port = 4000;
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://myproductionapp.com",
+  "https://singular-cupcake-899478.netlify.app/",
 ];
 app.use(
   cors({
