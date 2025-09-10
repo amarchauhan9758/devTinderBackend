9eac243c57d0fd4092addc712ab7585218f53205
amarchauhan9758 <aryanchauhan9758@gmail.com>
2025-09-11 01:04:07 +0530

    code comit afte  key secureedd

--- src/utils/razorpay.js
+++
@@ -1,8 +0,0 @@
-const Razorpay = require("razorpay");
-
-var instance = new Razorpay({
-  key_id: "rzp_test_R5ZVQ81Am4moZQ",
-  key_secret: "ptkwK0Ts67kXIFPSSTW12oRz",
-});
-
-module.exprots = instance;
